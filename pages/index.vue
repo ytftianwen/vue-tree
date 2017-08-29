f93db70e98d5df80ff5d29880672881c3312fad6
yangtaofeng <yangtaofeng@b.360.cn>
2017-08-29 17:44:44 +0800

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="container">
+    <tree :data="data" :root-data="data" :default-checked-keys="defaultCheckedKeys"></tree>
+  </section>
+</template>
+
+<script>
+import tree from '~/components/tree.vue'
+
+export default {
+  components: {
+    tree
+  },
+  data () {
+    return {
+      data: {
+        id: 1,
+        content: '一级 1',
+        children: [{
+          id: 4,
+          content: '二级1-1',
+          children: [{
+            id: 9,
+            content: '三级1-1-1'
+          }]
+        }]
+      },
+      defaultCheckedKeys: [4]
+    }
+  }
+}
+</script>
+<style>
+  .container {
+    margin: 20px;
+  }
+</style>
